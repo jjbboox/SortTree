2fa380df9ddebefd90cf47be8392926cc7a428ad
jjbboox <jjbboox@163.com>
2019-01-17 11:03:53 +0800

    删除每个方法大括弧外的;

--- SortTree.h
+++ SortTree.h
@@ -3,18 +3,18 @@
 
 class SortLeaf {
 	public:
-		SortLeaf(float fnum):value(fnum),parent(NULL), smallLeaf(NULL), bigLeaf(NULL) {};
+		SortLeaf(float fnum):value(fnum),parent(NULL), smallLeaf(NULL), bigLeaf(NULL) {}
 		~SortLeaf() {
 			if(smallLeaf != NULL) delete smallLeaf;
 			if(bigLeaf != NULL) delete bigLeaf;
-		};
-		float getValue() { return value;};
-		SortLeaf* getParent() { return parent;};
-		SortLeaf* getSmallLeaf() { return smallLeaf;};
-		SortLeaf* getBigLeaf() { return bigLeaf;};
-		void setParent(SortLeaf* leaf) { parent = leaf;};
-		void setSmallLeaf(SortLeaf* leaf) { smallLeaf = leaf;};
-		void setBigLeaf(SortLeaf* leaf) { bigLeaf = leaf;};
+		}
+		float getValue() { return value;}
+		SortLeaf* getParent() { return parent;}
+		SortLeaf* getSmallLeaf() { return smallLeaf;}
+		SortLeaf* getBigLeaf() { return bigLeaf;}
+		void setParent(SortLeaf* leaf) { parent = leaf;}
+		void setSmallLeaf(SortLeaf* leaf) { smallLeaf = leaf;}
+		void setBigLeaf(SortLeaf* leaf) { bigLeaf = leaf;}
 		void append(float fnum) {
 			if(fnum < value) {
 				if(smallLeaf != NULL) {
@@ -34,13 +34,12 @@ class SortLeaf {
 					bigLeaf->setParent(this);
 				}
 			}
-		};
+		}
 	private:
 		float value;
 		SortLeaf* parent;
 		SortLeaf* smallLeaf;
 		SortLeaf* bigLeaf;
 };
-
 
 #endif
